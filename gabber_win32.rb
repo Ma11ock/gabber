039c18b4e37fb27f8a8ef54f83f7de94720aa8e9
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-02-28 04:33:56 -0800

    Get the steam running ID from the registry

--- gabber_win32.rb
+++ gabber_win32.rb
@@ -2,6 +2,22 @@
 
 require 'win32/registry'
 
+# Steam registry key name.
+STEAM_KEYNAME = 'SOFTWARE\\Valve\\Steam'
+
+# Access rights.
+ACCESS = Win32::Registry::KEY_READ
+
+def get_game_id
+  begin
+    Win32::Registry::HKEY_CURRENT_USER.open(STEAM_KEYNAME, ACCESS)['RunningAppID'] or
+      raise 'No RunningAppID could be found from the registry! :/'
+  rescue Win32::Registry::Error => e
+    raise "Registry error: maybe steam isn\'t running? #{e.message}"
+  end
+end
+
 def init_gabber
-  puts "Gab!"
+  # For now just print the game ID.
+  puts get_game_id
 end
