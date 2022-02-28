a7745e2a0c28e388a23ed12fc7daee9ce9564e1d
Ryan Jeffrey <ryan@ryanmj.xyz>
2022-02-28 03:20:07 -0800

    Initial commit.

---
+++ gabber.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+# Gabber is a 
+
+
+case RUBY_PLATFORM
+when /cygwin|mswin|mingw|bccwin|wince|emx/
+  require './gabber_win32'
+when /linux/
+  require './gabber_linux'
+when /bsd/
+# TODO
+when /darwin|mac/
+# TODO
+else
+  raise 'Operating system not supported ! :/'
+end
+
+init_gabber
---
+++ gabber_win32.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require 'win32/registry'
+
+def init_gabber
+  puts "Gab!"
+end
